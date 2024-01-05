30bd2237ff2571b2f90ffa01abfb17c9a1e59c15
J-Doc <jdoc@mail.com>
2024-01-04 21:34:26 -0500

    Add small angle steering and log difficulty ahead

--- custom_files/reward_function.py
+++ custom_files/reward_function.py
@@ -1,23 +1,27 @@
 import math
 
-LAST_PROGRESS   = 0.0
+LAST_PROGRESS = 0.0
 
 
 def get_next_distinct_index(i, waypoints):
-    '''
+    """
     Get the next distinct waypoint index in a path, after i (skip duplicate waypoints)
-    '''
+    """
     ahead = i + 1
     if i == len(waypoints) - 1:
         ahead = 0
-    if waypoints[i][0] == waypoints[ahead][0] and waypoints[i][1] == waypoints[ahead][1]:
+    if (
+        waypoints[i][0] == waypoints[ahead][0]
+        and waypoints[i][1] == waypoints[ahead][1]
+    ):
         return get_next_distinct_index(ahead, waypoints)
     return ahead
 
+
 def get_prev_distinct_index(i, waypoints):
-    '''
+    """
     Get the previous distinct waypoint index in a path, before i (skip duplicate waypoints)
-    '''
+    """
     prev = i - 1
     if i == 0:
         prev = len(waypoints) - 1
@@ -25,10 +29,11 @@ def get_prev_distinct_index(i, waypoints):
         return get_prev_distinct_index(prev, waypoints)
     return prev
 
+
 def get_direction(i, waypoints):
-    '''
+    """
     Get direction at waypoint i
-    '''
+    """
     behind = get_prev_distinct_index(i, waypoints)
     ahead = get_next_distinct_index(i, waypoints)
     behindx = (waypoints[i][0] + waypoints[behind][0]) / 2.0
@@ -41,6 +46,7 @@ def get_direction(i, waypoints):
     diffy = ahead[1] - behind[1]
     return math.atan2(diffy, diffx)
 
+
 def get_direction_change(i, waypoints):
     behind = get_prev_distinct_index(i, waypoints)
     ahead = get_next_distinct_index(i, waypoints)
@@ -51,32 +57,36 @@ def get_direction_change(i, waypoints):
     diff1 = math.atan2(math.sin(diff1), math.cos(diff1))
     return (diff1 + diff0) / 2.0
 
+
 def reward_function(params):
-    '''
+    """
     Progress with track difficulty
-    '''
+    """
 
     global LAST_PROGRESS
     difficulty_factor = 5
-    # look_ahead = 3
+    look_ahead = 3
 
-    #Obtain difficulty
-    curve      = get_direction_change(params['closest_waypoints'][0], params['waypoints'])
+    # Obtain difficulty
+    curve = get_direction_change(params["closest_waypoints"][0], params["waypoints"])
     difficulty = 1 + float(difficulty_factor * abs(curve))
-    progress   = params['progress']
+    progress = params["progress"]
 
-    # point_ahead = params['closest_waypoints'][1]
-    # for _ in range(look_ahead):
-    #     point_ahead = get_next_distinct_index(point_ahead, params['waypoints'])
+    point_ahead = params["closest_waypoints"][1]
+    difficulty_ahead = 0
+    for _ in range(look_ahead):
+        difficulty_ahead += abs(get_direction_change(point_ahead, params["waypoints"]))
+        point_ahead = get_next_distinct_index(point_ahead, params["waypoints"])
 
-    #Base reward
-    step_progress   = progress - LAST_PROGRESS
-    LAST_PROGRESS   = progress
+    # Base reward
+    step_progress = progress - LAST_PROGRESS
+    LAST_PROGRESS = progress
 
     if step_progress < 0:
         return float(0.00001)
 
     weighted = float((5.0 * step_progress) ** 1.75)
     print(f"MY_TRACE_LOG:{params['steps']},{progress}")
+    print(f"MY_DEBUG_LOG:{difficulty_ahead},{params['speed']}")
 
     return float(weighted * difficulty)
